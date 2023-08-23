36e08dd5988ef10682b39b73cf2215533f7ddde6
Joshua Zobule <64586778+thisisjoshu@users.noreply.github.com>
2023-08-23 14:44:38 +1100

    added routing for login info

--- auth-reg-client/src/components/LoginInfo.js
+++ auth-reg-client/src/components/LoginInfo.js
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import "../styles/LoginInfo.css";
 
 function LoginInfo() {
   return (
     <div className="login-info">
       <span className="login-info-text">Already have an account? </span>
-      <span className="login-info-text-green">Login</span>
+      <Link to="/login">
+        <span className="login-info-text-green">Login</span>
+      </Link>
     </div>
   );
 }
