58a11a5d9aed56e1210bca9737f968f6dd4495e6
Joshua Zobule <64586778+thisisjoshu@users.noreply.github.com>
2023-08-24 19:15:06 +1100

    some sea ess ess

--- auth-reg-client/src/components/RegAlert.js
+++ auth-reg-client/src/components/RegAlert.js
@@ -1,14 +1,16 @@
+import "../styles/RegAlert.css"
+
 function RegAlert(props) {
   if (props.emailErrorMessage) {
     return (
-      <div className="alert-error">
-        <span className="alert-text">{props.emailErrorMessage}</span>
+      <div className="reg-alert-error">
+        <span className="reg-alert-text">{props.emailErrorMessage}</span>
       </div>
     );
   } else if (props.usernameErrorMessage) {
     return (
-      <div className="alert-error">
-        <span className="alert-text">{props.usernameErrorMessage}</span>
+      <div className="reg-alert-error">
+        <span className="reg-alert-text">{props.usernameErrorMessage}</span>
       </div>
     );
   }
