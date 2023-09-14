8bc28a493b556c0d7dabe5ab10d469b556b30a83
Geolynx Saemane <gsaemane@genisys.com.sb>
2023-09-14 13:51:03 +1100

    View updates
    frontend update
    +verification notify [page] - styles + html : done
    +verification success [page] - styles +html : done
    +verification timeout [page] - styles + html: done
    +dashboard to include profile menu  & logout functionality : done
    +update password [component] - styles + html: done

--- auth-reg-client/src/pages/VerificationNotify.js
+++ auth-reg-client/src/pages/VerificationNotify.js
@@ -1,5 +1,22 @@
+import "../styles/VerificationNotify.css";
 function VerificationNotify(){
 
+    return(
+        <div className="verification-notify">
+            <div className="title">
+                <h1>Verification is needed</h1>
+            </div>
+            <div className="info">
+                Hi _user_ we have sent a verification email to your inbox _email_, please click on the 
+                verification link in your inbox to activate your account
+                <br/>
+                Link will expire in 30 minutes
+
+            </div>
+
+        </div>
+
+    );
 
 }
 
--- auth-reg-client/src/pages/VerificationTimeout.js
+++ auth-reg-client/src/pages/VerificationTimeout.js
@@ -1,6 +1,29 @@
+import { Link } from "react-router-dom";
+import "../styles/VerificationTimeout.css";
 function VerificationTimeout(){
 
+    
+    return(
 
+        <div className="timeoutContainer">
+            
+            <div className="title">
+                <h1>Verification Timeout</h1>
+            </div>
+            <div className="info">
+                Sorry we didn't get your verification after 30mins.
+            </div>
+            <Link to="/verification">
+            <input
+                type="button"
+                name="resendLink"
+                className="resendLink-button"
+                value="Resend Link"
+              
+            />
+            </Link>
+        </div>
+    );
 }
 
 export default VerificationTimeout;
